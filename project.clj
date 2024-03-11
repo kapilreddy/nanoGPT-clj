e832044c84d7173f9f1b65d9ec83df94fd16ca1f
Kapil Reddy <reddy.kapil@gmail.com>
2024-03-11 19:18:40 +0530

    Add clojure repo

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject nanoGPT "0.0.1"
+  :author "Kapil Reddy <https://www.kapilreddy.me>"
+  :description "An implementation of nanoGPT in Clojure with some Java"
+
+  :dependencies
+  [[org.clojure/clojure "1.11.2"]]
+
+  )
---
+++ src/nanoGPT-clj/core.clj
@@ -0,0 +1,1 @@
+(ns nanoGPT-clj.core)
