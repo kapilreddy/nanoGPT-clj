6e99fb792b5cd95508b0bdc5c5551a3850b1c226
Kapil Reddy <reddy.kapil@gmail.com>
2024-03-11 19:24:19 +0530

    Add character tokenizer

--- src/nanoGPT-clj/core.clj
+++ src/nanoGPT-clj/core.clj
@@ -1,1 +1,20 @@
 (ns nanoGPT-clj.core)
+
+
+;; A simple character based tokenizer
+;; @TODO Replace with something similar to tiktoken
+;; @TODO Create a protocol for tokenizer
+
+(defn encode
+  [in]
+  (map int in))
+
+(defn decode
+  [in]
+  (apply str (map char in)))
+
+
+(comment
+  (let [x "hello world"]
+    (= (decode (encode x))
+       x)))
